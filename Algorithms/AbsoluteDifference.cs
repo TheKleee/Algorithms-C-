49f0797548b88905c4f5775fd40e88eb33d632e8
Marko Kostic <52660186+TheKleee@users.noreply.github.com>
2022-05-04 22:11:39 +0200

    Update AbsoluteDifference.cs

--- Algorithms/AbsoluteDifference.cs
+++ Algorithms/AbsoluteDifference.cs
@@ -1,5 +1,6 @@
 //Write a C# Sharp program to get the absolute difference between n and 51. If n is greater than 51 return triple the absolute difference.
 
+//With Math methods:
 using System;
 
 public class AbsoluteDifference
@@ -13,5 +14,23 @@ public class AbsoluteDifference
     public static int AbsoluteDifference(int n)
     {
         return n > 51 ? 3 * Math.Abs(n - 51) : Math.Abs(n - 51);
+    }
+}
+
+//Without Math methods
+public class AbsoluteDifferenceSimplified
+{
+    public static void Main()
+    {
+        System.Console.WriteLine("Absolute difference of 3 and 51 is: " + AbsoluteDifference(3));
+        System.Console.WriteLine("Absolute difference of 57 and 51 is not: " + AbsoluteDifference(57));
+    }
+    
+    public static int AbsoluteDifference(int n)
+    {
+        int dif = 51; //The desired difference point
+        
+        if(n > dif) return (n - dif) * 3;
+        else return dif - n;
     }
 }
