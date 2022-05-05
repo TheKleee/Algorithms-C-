cb9e0d14d155291ba13dba24d90cda5c822ffdb5
Marko Kostic <52660186+TheKleee@users.noreply.github.com>
2022-05-05 12:40:21 +0200

    Update BoolThrity.cs

--- Algorithms/BoolThrity.cs
+++ Algorithms/BoolThrity.cs
@@ -14,4 +14,21 @@ public class BoolThrity
         if(x == 30 || y == 30) return true;
         return x + y == 30 ? true : false;
     }
+
+}
+
+//Simplified:  
+public class BoolThirtySimple
+{
+     public static void Main(string[] args)
+     {
+        System.Console.WriteLine("3 + 27 is 30? " + ThirtyCheckSimplified(3, 27));
+        System.Console.WriteLine("Either 35 or 30 is actually 30? " + ThirtyCheckSimplified(35, 30));
+        System.Console.WriteLine("3 + 35 is 30? " + ThirtyCheckSimplified(3, 35));
+     }
+
+    public static bool ThirtyCheckSimplified(int x, int y)
+    {
+        return x == 30 || y == 30 || x + y == 30;
+    }
 }
