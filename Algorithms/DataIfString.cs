042fce33cbaa2c76238a8d0c4b92278b73c68457
Marko Kostic <52660186+TheKleee@users.noreply.github.com>
2022-05-05 13:23:19 +0200

    Create DataIfString.cs

---
+++ Algorithms/DataIfString.cs
@@ -0,0 +1,31 @@
+using System;
+
+public class DataIfString
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"Data starts with if: {IfString("iffy")}");
+        Console.WriteLine($"Data doesn't start with if: {IfString("fly")}");
+    }
+
+    private static string IfString(string data)
+    {
+        if (data[0] == 'i' && data[1] == 'f') return data;
+        return $"if{data}";
+    }
+}
+
+public class DataIfStringSubstring
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"Data starts with if: {IfStringSubstring("iffy")}");
+        Console.WriteLine($"Data doesn't start with if: {IfStringSubstring("fly")}");
+    }
+
+    private static string IfStringSubstring(string data)
+    {
+        if (data.Length > 2 && data.Substring(0, 2).Equals("if")) return data;
+        return $"if{data}";
+    }
+}
