bbecab8df585b9784543a6c3d147f53ce824bf61
Marko Kostic <52660186+TheKleee@users.noreply.github.com>
2022-05-09 10:31:34 +0200

    Create ModuleCheck.cs

---
+++ Algorithms/Day 5/ModuleCheck.cs
@@ -0,0 +1,21 @@
+/*
+ * Write a C# Sharp program to check if a given number is within 2 of a multiple of 10.
+ */
+
+using System;
+
+//With Math methods
+public class ModuleCheck
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"{ModuleTenRangeTwo(100)}");
+        Console.WriteLine($"{ModuleTenRangeTwo(21)}");
+        Console.WriteLine($"{ModuleTenRangeTwo(52)}");
+        Console.WriteLine($"{ModuleTenRangeTwo(7)}");
+    }
+    public static bool ModuleTenRangeTwo(int x)
+    {
+        return x % 10 <= 2 || x % 10 >= 8;
+    }
+}
