fa3795390026147f855063f8385fb1186149aabd
Marko Kostic <52660186+TheKleee@users.noreply.github.com>
2022-05-16 11:16:48 +0200

    Create CheckNum.cs

---
+++ Day 12/CheckNum.cs
@@ -0,0 +1,29 @@
+/*
+ *  Write a C# Sharp program to check if a given array of integers contains 5 next to a 5 somewhere.
+ */
+
+using System;
+
+public class CheckNum
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(FiveNextToFive(new[] { 2, 5, 6, 5, 17 }));
+        Console.WriteLine(FiveNextToFive(new[] { 21, 6, 1234 }));
+        Console.WriteLine(FiveNextToFive(new[] { 21, 5, 5 }));
+        Console.WriteLine(FiveNextToFive(new[] { 5, 5 }));
+    }
+
+    public static bool FiveNextToFive(int[] a)
+    {
+        bool fnf = false;
+        for (int i = 0; i < a.Length; i++)
+        {
+            fnf = i > 0 ?
+                a[i] == 5 && a[i - 1] == 5 :
+                false;
+            if (fnf) break;
+        }
+        return fnf;
+    }
+}
