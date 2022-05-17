d1ea72292ca39cd3dafaccc341a0605078d797a6
Marko Kostic <52660186+TheKleee@users.noreply.github.com>
2022-05-17 13:52:40 +0200

    Create ConditionChecker.cs

---
+++ Algorithms/Day 13/ConditionChecker.cs
@@ -0,0 +1,29 @@
+/*
+ * Write a C# Sharp program to check a given array of integers and return true if the given array contains two 5's next to each other,
+ * or two  5 separated by one element.
+ */
+
+using System;
+
+public abstract class ConditionChecker
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(FiveToFive(new[] { 2, 5, 6, 5, 17 }));
+        Console.WriteLine(FiveToFive(new[] { 2, 5, 5, 17 }));
+
+    }
+
+    private static bool FiveToFive(int[] a)
+    {
+        bool fiveToFive = false;
+        if (a.Length > 1)
+            for (int i = 0; i < a.Length; i++)
+                if (a[i] == 5 && a[i - 1] == 5)
+                {
+                    fiveToFive = true;
+                    break;
+                }
+        return fiveToFive;
+    }
+}
