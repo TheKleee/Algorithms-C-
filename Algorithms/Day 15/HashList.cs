9b87de7d42e343964fce3575c926661a1bca78fd
Marko Kostic <52660186+TheKleee@users.noreply.github.com>
2022-05-19 13:08:22 +0200

    Create HashList.cs

---
+++ Algorithms/Day 15/HashList.cs
@@ -0,0 +1,72 @@
+/*
+ * Write a C# Sharp program to create a new list from a given list of strings where each element has
+ * "#" added at the beginning and end position.
+ */
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using ListEditor;
+
+#region Area where Junior can change things:
+public static class HashList
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            List<string> s1 = new List<string>() { "1", "2", "3" }.NestElemInHash();
+            List<string> s2 = new List<string>() { "z", "x", "c" }.NestElemInHash();
+            List<string> s3 = new List<string>() { "!", "@", "#" }.NestElemInHash();
+            ReadList.DisplayMultipleLists(new List<List<string>> { s1, s2, s3 });
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"{e.Message}\n{e.StackTrace}");
+        }
+    }
+}
+#endregion junior's influence ends here />
+
+
+#region Junior can't touch this:
+namespace ListEditor
+{
+    public static class ReturnList
+    {
+        #region String Lists:
+        public static List<string> NestElemInHash(this List<string> l)
+        {
+            if (l == null)
+                throw new ArgumentNullException("The given list must contain elements!");
+
+            IEnumerable<string> s = l.Select(x => "#" + x + "#");
+            return s.ToList();
+        }
+        #endregion string lists />
+    }
+
+    public static class ReadList
+    {
+        #region String List:
+        public static void DisplayList(this List<string> l)
+        {
+            if (l.Count < 1 || l == null)
+                throw new ArgumentNullException("The given list must contain at least one element!");
+
+            foreach (string r in l) Console.Write($"{r} ");
+            Console.WriteLine("");
+        }
+
+        public static void DisplayMultipleLists(this List<List<string>> multiList)
+        {
+            if (multiList.Count < 1 || multiList == null)
+                throw new ArgumentNullException("The given jagged list must contain at least one list as an element!");
+
+            foreach (List<string> mA in multiList)
+                mA.DisplayList();
+        }
+        #endregion string list />
+    }
+}
+#endregion ends here />
